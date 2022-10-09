6c6b6b30939af2d9876cf67e4b20eed3adb58a7b
seonb2n <626ksb@gmail.com>
2022-10-09 19:55:46 +0900

    pjt create

---
+++ src/main/java/com/example/batchpjt/BatchpjtApplication.java
@@ -0,0 +1,13 @@
+package com.example.batchpjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchpjtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchpjtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/batchpjt/BatchpjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batchpjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchpjtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
